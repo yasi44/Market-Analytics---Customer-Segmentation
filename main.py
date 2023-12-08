d4de2c01aae4fe396ff08c79de7f68dbb2cef830
yasim <yasi.mailak@gmail.com>
2023-12-07 19:04:46 -0800

    Customer Segmentation done on both Univariant and Bivariant

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+''''
+customer segmentation helps in different marketing strategy
+'''
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import warnings
+
+warnings.filterwarnings('ignore')
